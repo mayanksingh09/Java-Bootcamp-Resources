a6ba0ae5effdfd10386612c7b9b23fe0496f9cd9
mayanksingh09 <mayanksingh@grubhub.com>
2024-01-22 05:47:52 +0530

    Added Workbook2.4 changes

--- Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
+++ Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -1,5 +1,12 @@
 public class YourInitials {
     public static void main(String[] args) {
          // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+         System.out.println("M   M   SSS ");
+         System.out.println("MM MM  S   S");
+         System.out.println("M M M  S    ");
+         System.out.println("M   M   SSS ");
+         System.out.println("M   M      S");
+         System.out.println("M   M  S   S");
+         System.out.println("M   M   SSS ");
     }
 }
